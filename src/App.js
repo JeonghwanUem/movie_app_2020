e9191b76b8d2d3fa55519c8c7b62a3a690b3811a
Jeonghwan Uem <uem2530@gmail.com>
2020-05-05 23:22:42 +0900

    jsx

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+
+
+const foodILike = [
+  {
+  id:1,
+  name:"Kimchi",
+  image:"https://www.maangchi.com/wp-content/uploads/2019/11/vegankimchi-insta-650x650.jpg"
+},
+{
+  id:2,
+  name:"Samgyopsal",
+  image:
+    "https://i2.wp.com/www.gildedgingerbread.com/wp-content/uploads/2017/08/Samgyeopsal-1.jpg?resize=750%2C600&ssl=1"
+}
+];
+
+function Food({name , picture}){
+  return <div> 
+  <h1>I Like {name}</h1>
+  <img src = {picture} alt={name}></img>
+  </div>
+  }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="App">
+    {foodILike.map(dish =>( 
+    <Food key={dish.id} name={dish.name} picture = {dish.image}/>
+    ))}
+  </div>
   );
-}
+  }
 
 export default App;
