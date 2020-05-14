51569271c4a46602d093469bd6416b8013013d99
Jeonghwan Uem <uem2530@gmail.com>
2020-05-14 23:52:56 +0900

    git io

--- src/Movie.js
+++ src/Movie.js
@@ -1,20 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
-import "./Movie.css"
+import "./Movie.css";
 
-function Movie({id,title,summary,poster,year,genres}){
-return <div className ="movie">
-    <img src ={poster} alt ={title} title ={title}/>
-    <div className ="movieData">
-    <h3 className ="movie_title" >{title}</h3>
-    
-    <h5 className = "movie_title">{year}</h5>
-<ul className = "genres">{genres.map((genre,index)=>
-<li key={index} className="genres__genre">{genre}</li>)}
-</ul>
-    <p className= "movie_summary">{summary}</p>
+function Movie({ year, title, summary, poster, genres }) {
+  return (
+    <div className="movie">
+      <img src={poster} alt={title} title={title} />
+      <div className="movie__data">
+        <h3 className="movie__title">{title}</h3>
+        <h5 className="movie__year">{year}</h5>
+        <ul className="movie__genres">
+          {genres.map((genre, index) => (
+            <li key={index} className="genres__genre">
+              {genre}
+            </li>
+          ))}
+        </ul>
+        <p className="movie__summary">{summary.slice(0,100)}...</p>
+      </div>
     </div>
-</div>
+  );
 }
 //style={{backgroundColor:"red"}}
 /**Movie.PropTypes = {
